5f2094a19afe0762fcfa1bdc069cd2293133e2b4
Whix100 <whix100@gmail.com>
2022-06-13 18:10:14 -0400

    Created IColor
    Created the basic IColor interface that all colors will be extending.

---
+++ ColorSpaces/Colors/IColor.cs
@@ -0,0 +1,20 @@
+namespace ColorSpaces.Colors;
+
+public interface IColor : IEquatable<Color>, IEquatable<KnownColor>, IEquatable<IColor>
+{
+    /// <summary>
+    /// <c>true</c> if the current color is in it's normal form, <c>false</c> otherwise.
+    /// </summary>
+    public bool IsNormalized
+    {
+        get;
+    }
+
+    /// <summary>
+    /// Hexadecimal color code for the current color.
+    /// </summary>
+    public uint Hex
+    {
+        get;
+    }
+}
