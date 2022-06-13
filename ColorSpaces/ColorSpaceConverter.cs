75d3de462dcb0873327fa313b38dcffae03a444a
Whix100 <whix100@gmail.com>
2022-06-13 18:34:38 -0400

    Added ColorConverter and ErrorUtils
    Added ColorConverter to convert between color spaces and ErrorUtils to make error processing more modular.

---
+++ ColorSpaces/ColorSpaceConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace ColorSpaces;
+
+internal static class ColorSpaceConverter
+{
+    /// <summary>
+    /// Converts the 8-bit red, green, and blue values to a hexadecimal unsigned integer color code.
+    /// </summary>
+    /// <param name="red">The red value of the color.</param>
+    /// <param name="green">The green value of the color.</param>
+    /// <param name="blue">The blue value of the color.</param>
+    /// <returns>
+    /// A uint hexadecimal value of the color calculated from <c>red</c>, <c>green</c>, and <c>blue</c>.
+    /// </returns>
+    internal static uint ConvertRGBToHexadecimal(byte red, byte green, byte blue)
+        => (uint)(red << 16 | green << 8 | blue);
+
+    /// <summary>
+    /// Converts the unsigned integer hexadecimal color code to 8-bit red, green, and blue values.
+    /// </summary>
+    /// <param name="hex">The hexadecimal color code of the color.</param>
+    /// <returns>
+    /// A byte[] containing the red, green, and blue values of the color calculated from <c>hex</c>.
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when the value of <c>hex</c> is outside the range of 0x000000 to 0xffffff.
+    /// </exception>
+    internal static byte[] HexadecimalToRGB(uint hex)
+    {
+        ErrorUtils.CheckForArgumentOutOfRangeException(hex, 0, 0xFFFFFF, nameof(hex));
+
+        return new byte[] { (byte)((hex >> 16) & 0xFF), (byte)((hex >> 8) & 0xFF), (byte)(hex & 0xFF) };
+    }
+}
---
+++ ColorSpaces/ErrorUtils.cs
@@ -0,0 +1,27 @@
+﻿namespace ColorSpaces;
+
+internal static class ErrorUtils
+{
+    /// <summary>
+    /// Checks whether an IComparable is out of a range and throws an exception if it is.
+    /// </summary>
+    /// <param name="argument">The value to check.</param>
+    /// <param name="min_value">The minimum value of the range.</param>
+    /// <param name="max_value">The maximum value of the range.</param>
+    /// <param name="name">The name of the variable <c>argument</c> get's its value from.</param>
+    /// <param name="inclusive">Whether the range to check is inclusive or exclusive.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when the value of <c>argument</c> is outside the range determined by <c>min_value</c> and 
+    /// <c>max_value</c>.
+    /// </exception>
+    internal static void CheckForArgumentOutOfRangeException(IComparable argument, IComparable min_value,
+        IComparable max_value, string name, bool inclusive = true)
+    {
+        if (inclusive && (min_value.CompareTo(argument) < 1 || max_value.CompareTo(argument) > -1))
+            throw new ArgumentOutOfRangeException(name, argument, $"{name} must be a value greater than or equal to " +
+                $"{min_value} and less than or equal to {max_value}.");
+        else if (!inclusive && (min_value.CompareTo(argument) < 0 || max_value.CompareTo(argument) > 0))
+            throw new ArgumentOutOfRangeException(name, argument, $"{name} must be a value greater than {min_value} " +
+                $"and less than {max_value}.");
+    }
+}
